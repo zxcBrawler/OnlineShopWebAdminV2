22b2acbcfe7b255793b3eddf1472464e9dc77b8e
polinasav <postscriptum145@gmail.com>
2024-03-09 16:57:29 +0300

    some code refactoring

--- lib/feature/shared/presentation/widget/admin/users_table.dart
+++ lib/feature/shared/presentation/widget/admin/users_table.dart
@@ -3,8 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:xc_web_admin/core/widget/table/basic_data_source.dart';
 import 'package:xc_web_admin/core/widget/table/basic_table.dart';
 import 'package:xc_web_admin/core/widget/table/colums_generator.dart';
+import 'package:xc_web_admin/di/service.dart';
 import 'package:xc_web_admin/feature/shared/domain/entities/user_entity.dart';
 import 'package:xc_web_admin/feature/shared/presentation/bloc/user/user_bloc.dart';
+import 'package:xc_web_admin/feature/shared/presentation/bloc/user/user_event.dart';
 import 'package:xc_web_admin/feature/shared/presentation/bloc/user/user_state.dart';
 
 class UsersTable extends StatefulWidget {
@@ -24,37 +26,40 @@ class _UsersTableState extends State<UsersTable> {
   /// the runtime type of the state.
   Widget build(BuildContext context) {
     // Build the widget tree based on the state of the RemoteUserBloc
-    return BlocBuilder<RemoteUserBloc, RemoteUserState>(
-      // Builder method to render the widget based on the state of RemoteUserBloc
-      builder: (_, state) {
-        // Switch on the runtime type of the state to handle different cases
-        switch (state.runtimeType) {
-          // Case when the state is of type RemoteUserLoading
-          case RemoteUserLoading:
-            // Show loading indicator while fetching data
-            return const Center(child: CircularProgressIndicator());
-          // Case when the state is of type RemoteUserDone
-          case RemoteUserDone:
-            // Retrieve the properties of the first user in the list
-            final List<String> columnTitles =
-                state.users!.first.getProperties();
-            // Build the BasicTable widget with the generated columns and data
-            return BasicTable(
-              columns: generateColumns(columnTitles),
-              dataSource: BasicDataSource<UserEntity>(
-                rowsCount: state.users!.length,
-                columnTitles: columnTitles,
-                data: state.users!,
-              ),
-            );
-          // Case when the state is of type RemoteUserError
-          case RemoteUserError:
-            // Display error message
-            return const Text("error");
-        }
-        // Return an empty sized box as the default case
-        return const SizedBox();
-      },
+    return BlocProvider<RemoteUserBloc>(
+      create: (context) => service()..add(const GetUsers()),
+      child: BlocBuilder<RemoteUserBloc, RemoteUserState>(
+        // Builder method to render the widget based on the state of RemoteUserBloc
+        builder: (_, state) {
+          // Switch on the runtime type of the state to handle different cases
+          switch (state.runtimeType) {
+            // Case when the state is of type RemoteUserLoading
+            case RemoteUserLoading:
+              // Show loading indicator while fetching data
+              return const Center(child: CircularProgressIndicator());
+            // Case when the state is of type RemoteUserDone
+            case RemoteUserDone:
+              // Retrieve the properties of the first user in the list
+              final List<String> columnTitles =
+                  state.users!.first.getProperties();
+              // Build the BasicTable widget with the generated columns and data
+              return BasicTable(
+                columns: generateColumns(columnTitles),
+                dataSource: BasicDataSource<UserEntity>(
+                  rowsCount: state.users!.length,
+                  columnTitles: columnTitles,
+                  data: state.users!,
+                ),
+              );
+            // Case when the state is of type RemoteUserError
+            case RemoteUserError:
+              // Display error message
+              return const Text("error");
+          }
+          // Return an empty sized box as the default case
+          return const SizedBox();
+        },
+      ),
     );
   }
 }
